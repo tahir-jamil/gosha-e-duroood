232e7897ffd614b811c4b5365075deac6ead454a
Muhammad Tahir <itstahirjamil@gmail.com>
2018-07-22 03:40:48 +0500

    signup getting form data

--- app/signup/signup.component.ts
+++ app/signup/signup.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RemoteUser } from '~/signup/user-data-service';
 import { Page } from 'tns-core-modules/ui/page/page';
+import { RadDataFormComponent } from 'nativescript-ui-dataform/angular/dataform-directives';
 
 @Component({
   selector: 'app-signup',
@@ -12,15 +13,26 @@ import { Page } from 'tns-core-modules/ui/page/page';
 export class SignupComponent implements OnInit {
 
   private _user: RemoteUser;
+  @ViewChild('myCommitDataForm') myCommitDataFormComp: RadDataFormComponent;
+
   constructor(private _page: Page) {
   }
 
   ngOnInit() {
-    this._user = new RemoteUser( "" , "" , "" , "" , "" , "" , "" , "" , "" , "" , "" , "" ,"",  0,0,0, true,);
+    this._user = new RemoteUser("", "", "", "", "", "", "", "", "", "", "", "", "", 0, 0, 0, true, );
   }
+  
   get user(): RemoteUser {
     return this._user;
   }
-  options_edu=["Post Graduation", "Under Graduation ","Graduation", "Matriculation","Diploma" ];
+
+  options_edu = ["Post Graduation", "Under Graduation ", "Graduation", "Matriculation", "Diploma"];
+
+  onPropertyCommitted() {
+    if (this.myCommitDataFormComp.dataForm.editedObject) {
+        console.dir(this.myCommitDataFormComp.dataForm);
+    }
+
+  }
 
 }
